15232f4c31da1cfa8236dec3c114d81d0411bc87
unknown <e101395@SSXP38352C.cns.cranfield.ac.uk>
2012-09-10 09:51:50 +0100

    Added compress_output

--- classes/parser.class.php
+++ classes/parser.class.php
@@ -11,7 +11,16 @@ class parser
         $html = preg_replace("'<style[^>]*>.*</style>'siU", '', $html);
         $search = array('@<script[^>]*?>.*?</script>@si', '@<[\/\!]*?[^<>]*?>@si', '@<style[^>]*?>.*?</style>@siU', '@<![\s\S]*?--[ \t\n\r]*>@');
         $text = preg_replace($search, '', $html);
-        return $text;
+        return $this->compress_output($text);
+    }
+
+    function compress_output($buffer)
+    {
+
+        $search = array('/\>[^\S ]+/s', '/[^\S ]+\</s', '/(\s)+/s');
+        $replace = array('>','<','\\1');
+        $buffer = preg_replace($search, $replace, $buffer);
+        return $buffer;
     }
 
 	function __destruct()
